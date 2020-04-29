cc7def980968124b1b8f42f07ef20719940bdad4
Pitagoras <Pitagoras@Finn3D>
2020-04-29 11:05:32 -0600

    Puede ser cualquier cosa

---
+++ src/SUMA.java
@@ -0,0 +1,12 @@
+
+public class SUMA {
+    public void Sumar(){
+            System.out.println("No se puede realizar la suma por falta de parámetros");    
+        }
+    public void Sumar(int num1, int num2){
+        
+            System.out.println("La suma es: "+(num1+num2));    
+        
+    }
+    
+}
